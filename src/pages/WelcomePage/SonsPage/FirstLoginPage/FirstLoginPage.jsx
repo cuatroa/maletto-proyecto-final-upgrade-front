ead795f3b6017b85b84e3f2517976e063b6f7a25
Rocio_Escobar <rociodelgadoesc@gmail.com>
2020-11-03 11:50:44 +0100

    structure front

---
+++ src/pages/WelcomePage/SonsPage/FirstLoginPage/FirstLoginPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FirstLoginPage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function WelcomePage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
