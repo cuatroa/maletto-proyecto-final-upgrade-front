e4d43ea62163e32a240e46efffbd8e3d7ee08e4a
Rocio_Escobar <rociodelgadoesc@gmail.com>
2020-11-03 17:11:28 +0100

    structure fine

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return( 
-  
-    <div></div>
-    
-    );
+  return (
+    <div>
+      <p>Hiii</p>
+    </div>
+  );
 }
 
 export default App;
