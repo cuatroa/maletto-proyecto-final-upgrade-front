bd172b9793c12ebf3253722389c68ca3f97b5efb
coritah44 <luranimal@gmail.com>
2020-11-07 12:41:00 +0100

    enlazar con back hecho y routing basico y img visualization

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import ChatPage from "./pages/ChatPage/ChatPage";
+import HomePage from "./pages/HomePage/HomePage";
+import Navbar from "./shared/components/Navbar/Navbar";
 
 function App() {
   return (
-    <div>
-      <p>Hiii</p>
+    <div className="App">
+      <header className="App-header">
+        <Router>
+          <div>
+            <Switch>
+              <Route path="/usuario">
+                <HomePage />
+              </Route>
+              <Route path="/chat">
+                <ChatPage />
+              </Route>
+              <Route path="/buscar">
+                <HomePage />
+              </Route>
+              <Route path="/">
+                <HomePage />
+              </Route>
+            </Switch>
+            <Navbar />
+          </div>
+        </Router>
+      </header>
     </div>
   );
 }
