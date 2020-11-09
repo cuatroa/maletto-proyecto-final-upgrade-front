29a4578d36ce7b111b791e56bede99c122dea0be
Cuatroa <adrianandres1992@gmail.com>
2020-11-09 12:13:02 +0100

    FONT Maletto-iconos agregada, Navbar terminado

--- src/shared/components/Navbar/Navbar.jsx
+++ src/shared/components/Navbar/Navbar.jsx
@@ -6,10 +6,10 @@ import './Navbar.scss';
 export default function Navbar() {
     return (
         <nav className="c-navbar">
-             <Link className="c-navbar__element" to="/">Home</Link>
-             <Link className="c-navbar__element">Buscar</Link>
-             <Link className="c-navbar__element">Chat</Link>
-             <Link className="c-navbar__element">Usuario</Link>
+             <Link className="c-navbar__element icon-home" to="/"></Link>
+             <Link className="c-navbar__element icon-search"></Link>
+             <Link className="c-navbar__element icon-bubbles4"></Link>
+             <Link className="c-navbar__element icon-user"></Link>
         </nav>
     )
 }
