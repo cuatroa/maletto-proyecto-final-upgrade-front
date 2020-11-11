08e05dbad50dba0a7fc835b96778dc4ee588118e
coritah44 <luranimal@gmail.com>
2020-11-11 11:09:02 +0100

    /search mapbox

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,138 +1,95 @@
-import React from "react";
-import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+import { findByLabelText } from "@testing-library/react";
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import Navbar from "../../shared/components/Navbar/Navbar";
-import usePlacesAutocomplete, {
-  getGeocode,
-  getLatLng,
-} from "use-places-autocomplete";
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxPopover,
-  ComboboxList,
-  ComboboxOption
-} from "@reach/combobox";
 import "./SearchPage.scss";
+import { Accordion, AccordionTab } from "primereact/accordion";
 
 
-
-
-
-function Map() {
-    const mapRef = React.useRef();
-    const onMapLoad = React.useCallback((map) => {
-      mapRef.current = map;
-    }, []);
-    const panTo = React.useCallback(({ lat, lng }) => {
-    mapRef.current.panTo({ lat, lng });
-    mapRef.current.setZoom(14);
-  }, []);
-  return (
-    <div>
-      <Search panTo={panTo}></Search>
-      <GoogleMap
-        defaultZoom={8}
-        defaultCenter={{ lat: -34.397, lng: 150.644 }}
-        onLoad={onMapLoad}
-      ></GoogleMap>
-    </div>
-  );
-}
-
-const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 export default function SearchPage() {
-  return (
-    <div>
-      {/* <Search */}
-      <div style={{ width: "100vw", height: "94vh" }}>
-        <WrappedMap
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `100%` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        />
-      </div>
-      <Navbar />
-    </div>
-  );
-}
-
-function Locate({ panTo }) {
-  return (
-    <button
-      className="locate"
-      onClick={() => {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            panTo({
-              lat: position.coords.latitude,
-              lng: position.coords.longitude,
-            });
-          },
-          () => null
-        );
-      }}
-    >
-      <img src="/compass.svg" alt="compass" />
-    </button>
-  );
-}
 
 
-
-
-function Search({ panTo }) {
-  const {
-    ready,
-    value,
-    suggestions: { status, data },
-    setValue,
-    clearSuggestions,
-  } = usePlacesAutocomplete({
-    requestOptions: {
-      location: { lat: () => -34.397, lng: () => 150.644 },
-      radius: 100 * 1000,
-    },
+  const [viewport, setViewport] = useState({
+    latitude: 45.4211,
+    longitude: -75.6903,
+    width: "100vw",
+    height: "60vh",
+    zoom: 6,
   });
 
-  // https://developers.google.com/maps/documentation/javascript/reference/places-autocomplete-service#AutocompletionRequest
-
-  const handleInput = (e) => {
-    setValue(e.target.value);
-  };
-
-  const handleSelect = async (address) => {
-    setValue(address, false);
-    clearSuggestions();
-
-    try {
-      const results = await getGeocode({ address });
-      const { lat, lng } = await getLatLng(results[0]);
-      panTo({ lat, lng });
-    } catch (error) {
-      console.log("😱 Error: ", error);
-    }
-  };
 
   return (
-    <div className="search">
-      <Combobox onSelect={handleSelect}>
-        <ComboboxInput
-          value={value}
-          onChange={handleInput}
-          disabled={!ready}
-          placeholder="Busca a tu guardián"
-        />
-        <ComboboxPopover>
-          <ComboboxList>
-            {status === "OK" &&
-              data.map(({ id, description }) => (
-                <ComboboxOption key={id} value={description} />
-              ))}
-          </ComboboxList>
-        </ComboboxPopover>
-      </Combobox>
+    <div className="general">
+      <div>
+        <ReactMapGL
+          {...viewport}
+          mapboxApiAccessToken={
+            "pk.eyJ1IjoiY29yaXRhaDQ0IiwiYSI6ImNraGMybzFtOTA2dzkydG1obG15Y2N5ejMifQ.gRgDwZ05972n1N3QPw1TpA"
+          }
+          onViewportChange={(viewport) => {
+            setViewport(viewport);
+          }}
+          mapStyle="mapbox://styles/coritah44/ckhcn7gg01n4a19o4kembizsi"
+        ></ReactMapGL>
+      </div>
+      <ul>
+        <li class="card">
+          <a
+            href="#"
+            class="featured-image"
+            style={{
+              backgroundImage:
+                "url(https://images.pexels.com/photos/5411784/pexels-photo-5411784.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260)",
+            }}
+          />
+          <article class="card-body">
+            <header>
+              <a href="utilidata-national-governors-association-meeting">
+                <div class="title">
+                  <h3>Sara García</h3>
+                  <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT621TlX12PE1LjWn4Djl1ZLj1-i0dR0I9OxA&usqp=CAU"
+                    alt=""
+                  />
+                  <div>
+                    <span class="icon-star-full"></span>
+                    <span class="icon-star-full"></span>
+                    <span class="icon-star-full"></span>
+                    <span class="icon-star-empty"></span>
+                    <span class="icon-star-empty"></span>
+                  </div>
+                </div>
+
+                <p class="meta">
+                  <span class="author">Utilidata</span>
+                  <span> | </span>
+                  <time class="updated" datetime="" itemprop="datePublished">
+                    July 27, 2017
+                  </time>
+                </p>
+              </a>
+            </header>
+          </article>
+        </li>
+      </ul>
+
+      {/* <div className="right-action">
+        <div className="text">
+          <h4>titulo1</h4>
+        </div>
+        <div>
+          <span className="icon-star-full"></span>
+        </div>
+      </div> */}
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <Navbar />
     </div>
   );
 }
