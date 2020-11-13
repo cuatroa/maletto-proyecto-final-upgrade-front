c2b4d77e23ab9e3386ab4306cee75ef534a8dea8
CarlosAnegon <carlosanegon1@gmail.com>
2020-11-13 12:48:51 +0100

    User Page Preview develop

--- src/pages/UserPage/UserBookingPage/UserBookingPage.jsx
+++ src/pages/UserPage/UserBookingPage/UserBookingPage.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import UserPage from "../UserPage";
-import Navbar from "./../../../shared/components/Navbar/Navbar"
+// import UserPage from "../UserPage";
+import Navbar from "./../../../shared/components/Navbar/Navbar";
 import "./UserBookingPage.scss"
 
 export default function UserBookingPage() {
