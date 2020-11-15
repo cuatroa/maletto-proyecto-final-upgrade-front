018f5a2d44c423b12befb92edf7a324870f03022
CarlosAnegon <carlosanegon1@gmail.com>
2020-11-15 11:05:06 +0100

    Publish Page + Guardian Page, Preview

--- src/pages/GuardianPage/SonsPage/PublishPage/PublishPage.jsx
+++ src/pages/GuardianPage/SonsPage/PublishPage/PublishPage.jsx
@@ -2,55 +2,79 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Navbar from "./../../../../shared/components/Navbar/Navbar";
-import "./PublishPage.scss"
+import "./PublishPage.scss";
 
 export default function PublishPage() {
-
-
-
   return (
     <div className="container">
+      <div className="container-noNavbar">
+      <Link className="icon-atras" to="/guardian" >  </Link>
+      
       <div className="div-image-title">
         {/* {user.map((item, index) => { */}
         {/* return ( */}
         <div className="image-title">
           <div>
             <h2>Descríbenos tu espacio</h2>
-
-
           </div>
           <div>
             <div>
+              <div className="icon-descargar" to="/" /*"Poner redirección"*/>
+                {" "}
+              </div>
               <h3>Especifica tu propiedad</h3>
-              <Link className="icon-descargar" to="/"/*"Poner redirección"*/> </Link>
-              <h4 className="small-text">Seleccione una opción</h4>
               <hr />
             </div>
-            <div className="tabs">
-              <div className="tab">
-                <input type="checkbox" id="chck1" />
-                <label className="tab-label" for="chck1">
-                  Cómo debe ser tu maleta
-              </label>
-                <hr />
-                <div className="tab-content">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum,
-                  reiciendis!
-              </div>
+
+            <div className="row">
+              <div className="tabs">
+                <div className="tab">
+                  <input type="checkbox" id="chck1" />
+                  <label className="tab-label" for="chck1">
+                    Seleccione una opción &nbsp;
+                  </label>
+                  <hr />
+                  <div className="tab-content">Casa</div>
+                  <div className="tab-content">Hotel</div>
+                  <div className="tab-content">Establecimiento</div>
+                </div>
               </div>
             </div>
+
             <div>
-              <Link className="icon-descargar" to="/guardian-spaces-page"/*"Poner redirección"*/> </Link>
+              <div
+                className="icon-descargar"
+                to="/guardian-spaces-page" /*"Poner redirección"*/
+              >
+                {" "}
+              </div>
               <h3>¿Qué tipo de espacio?</h3>
-              <h4 className="small-text">Selecciona una opción</h4>
+
               <hr />
             </div>
+
+            <div className="row">
+              <div className="tabs">
+                <div className="tab">
+                  <input type="checkbox" id="chck2" />
+                  <label className="tab-label" for="chck2">
+                    Seleccione una opción &nbsp;
+                  </label>
+                  <hr />
+                  <div className="tab-content">Habitación</div>
+                  <div className="tab-content">Hall</div>
+                  <div className="tab-content">Trastero</div>
+                  <div className="tab-content">Buhardilla</div>
+                  <div className="tab-content">Garaje</div>
+                </div>
+              </div>
+            </div>
+
             <div>
               <h3>Ubicación</h3>
               <hr />
             </div>
             <div>
-
               <h3>Fotos</h3>
               <hr />
             </div>
@@ -66,14 +90,13 @@ export default function PublishPage() {
               <h3>Servicios</h3>
               <hr />
             </div>
-
           </div>
         </div>
         {/* ); */}
         {/* })} */}
       </div>
       <Navbar />
+      </div>
     </div>
-
   );
 }
