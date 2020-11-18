db13bf10d886a8e731ec5569b134c743afe9cc04
Rocio_Escobar <rociodelgadoesc@gmail.com>
2020-11-18 16:06:42 +0100

    chat and guardian done

--- src/pages/GuardianPage/SonsPage/GuardianSpacesPage/GuardianSpacesPage.jsx
+++ src/pages/GuardianPage/SonsPage/GuardianSpacesPage/GuardianSpacesPage.jsx
@@ -19,27 +19,28 @@ export default function GuardianSpacesPage() {
     return (
         <div className="container">
             <div className="container-noNavbar">
-           <Link className="icon-atras" to="/guardian" > </Link>
-            <div className="bookings">
+                <Link className="icon-atras" to="/guardian" > </Link>
                 <h1>Tus anuncios</h1>
                 <hr />
+                <div className="advertisements">
+                    {user.map((item, index) => {
+                        return (
+                            <div className="photoAdvertisements" key={index}>
+                                <h2>{item.title}</h2>
+                                <img className="image-location" src={item.img} alt="" />
+                                <Link className="icon-proximo" to=""/*"Poner redirección"*/> </Link>
+                                <hr />
+                            </div>
+                        );
+                    })}
+                </div>
+                <h2>Añade otro espacio</h2>
+                <Link className="icon-plus " to="/guardian/publish"> </Link>
             </div>
-            <div className="div-image-title">
-                {user.map((item, index) => {
-                    return (
-                        <div className="image-title" key={index}>
-                            <h2>{item.title}</h2>
-                            <img className="image-location"src={item.img} alt="" />
-                            <Link className="icon-proximo" to =""/*"Poner redirección"*/> </Link>
-                            <hr/>
-                        </div>
-                    );
-                })}
+            <div>
+                <Navbar />
             </div>
-            <h3>Añade otro espacio</h3>
-            <Link className="icon-plus " to="/guardian/publish"> </Link>
-            </div>
-            <Navbar />
+
         </div>
     );
 }
