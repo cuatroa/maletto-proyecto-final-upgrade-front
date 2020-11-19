bddf7d9db8d7353c83ed130e827096c4429482a6
Cuatroa <adrianandres1992@gmail.com>
2020-11-19 23:29:07 +0100

    cambios y actualización de novedades

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,7 @@ import UserEditPage from "./pages/UserPage/UserEditPage/UserEditPage";
 // import Login from "./pages/LoginPage/SonsPage/Login/Login";
 // import Register from "./pages/LoginPage/SonsPage/Register/Register";
 import BookingPetitionsPage from "./pages/GuardianPage/SonsPage/BookingPetitionPage/BookingPetitionsPage";
-<<<<<<< HEAD
-
-=======
 import BookingInfoPage from "./pages/UserPage/BookingInfoPage/BookingInfoPage";
->>>>>>> baecb106ca017008d109ed8c48805c9d2fe96644
 function App() {
   return (
     <div>
